73507ff6de543c61f43d089ca3d4bb0506c680a0
Max Trash <max@deadend.xyz>
2022-07-09 21:22:10 +0300

    Sprites part 1

--- globals.gd
+++ globals.gd
@@ -6,6 +6,8 @@ const TIME_STEP = 0.8
 const DIRECTION_RELATIVE = 3
 
 
+var head_texture : Texture = preload("res://sprites/sprites_v01/head.png")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
