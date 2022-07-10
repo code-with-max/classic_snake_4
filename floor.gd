db07abfc9b3e81b73369d4063b1852de269042b0
Max Trash <max@deadend.xyz>
2022-07-10 13:49:38 +0300

    Add sprite choosing functions
    Signed-off-by: Max Trash <max@deadend.xyz>

--- floor.gd
+++ floor.gd
@@ -3,16 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var self_texture : Texture = G.get_texture("tail")
+	var self_texture : Texture = G.get_texture("floor")
 	if self_texture:
 		$Sprite2D.set_texture(self_texture)
 		$Sprite2D.set_offset(Vector2(24, 24))
 		$Sprite2D.show()
 	else:
-		$ColorRect.set_color(Color.DARK_SLATE_BLUE)
+		$ColorRect.set_color(Color.DIM_GRAY)
 		$ColorRect.show()
-		
 
 
-func  show_num(num):
-	$Label.set_text(str(num))
--- wall.gd
+++ wall.gd
@@ -3,7 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ColorRect.set_color(Color.MEDIUM_PURPLE)
+	var self_texture : Texture = G.get_texture("wall")
+	if self_texture:
+		$Sprite2D.set_texture(self_texture)
+		$Sprite2D.set_offset(Vector2(24, 24))
+		$Sprite2D.show()
+	else:
+		$ColorRect.set_color(Color.MEDIUM_PURPLE)
+		$ColorRect.show()
 
 
 # For debug
