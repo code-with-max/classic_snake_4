d4cea2c4c4bd2e30d66479b9a0f3b94d2ba6607b
Max Trash <max@deadend.xyz>
2022-09-02 13:28:33 +0300

    add some code comments

--- gui.gd
+++ gui.gd
@@ -15,21 +15,24 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+#func _process(_delta):
+#	pass
 
-
+# Redrawing actual status game score
 func redraw_score_label():
 	%L_score_data.set_text(str(G.get_score()))
 
 
+# Game main menu settings
 func _on_b_exit_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
 	get_tree().quit()
 
 
 func _on_b_settings_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
 	redraw_b_menu_state()
@@ -37,11 +40,10 @@ func _on_b_settings_pressed():
 
 
 func _on_b_start_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
 	snake_pit = pit_scene.instantiate()
-#	$MC_center.visible = false
-#	$MC_center.set_visible(false)
 	G.reset_score()
 	G.game_paused = false
 	redraw_score_label()
@@ -51,10 +53,10 @@ func _on_b_start_pressed():
 	$MC_bottom/B_pause.show()
 	var start_x = %Pos2D.get_global_position().x - 312
 	var start_y = %Pos2D.get_global_position().y - 480
-#	snake_pit.set_position(Vector2(48, 96))
 	snake_pit.set_position(Vector2(start_x, start_y))
 	add_child(snake_pit)
 	snake_pit.pit_need_to_destroyed.connect(on_pit_is_destroyed)
+	# Play music
 	if G.game_music:
 		$ASP_music.play()
 
@@ -69,9 +71,12 @@ func on_pit_is_destroyed():
 	$MC_bottom/L_info.show()
 
 
+# Call pause menu
 func _on_b_pause_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
+	# Stop music
 	if G.game_music:
 		$ASP_music.stop()
 	G.game_paused = true
@@ -80,7 +85,9 @@ func _on_b_pause_pressed():
 	$MC_pause_menu.show()
 
 
+# Game pause menu section
 func _on_b_resume_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
 	$MC_pause_menu.hide()
@@ -89,30 +96,42 @@ func _on_b_resume_pressed():
 #	yield(get_tree().create_timer(1), "timeout") # For Godot 3.xx
 	await get_tree().create_timer(1).timeout
 	G.game_paused = false
+	# Play music
 	if G.game_music:
 		$ASP_music.play()
 
 
 func _on_b_gomenu_pressed():
+	# Play "click"
 	if G.game_sound:
 		$ASP_sound.play()
 	$MC_pause_menu.hide()
 	on_pit_is_destroyed()
-#	G.game_paused = false # Use on button start
 
 
+# Redrawing actual status buttons in config menu
 func redraw_b_menu_state():
 	%B_music.set_pressed(G.game_music)
 	%B_sound.set_pressed(G.game_sound)
 
 
+# Game config menu section
 func _on_b_menu_hide_pressed():
-	$MC_settings.hide() # Replace with function body.e()
+	# Play "click"
+	if G.game_sound:
+		$ASP_sound.play()
+	$MC_settings.hide()
 
 
 func _on_b_music_pressed():
+	# Play "click"
+	if G.game_sound:
+		$ASP_sound.play()
 	G.game_music = %B_music.is_pressed()
 
 
 func _on_b_sound_pressed():
+	# Play "click"
+	if G.game_sound:
+		$ASP_sound.play()
 	G.game_sound = %B_sound.is_pressed()
