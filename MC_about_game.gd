8c92f1c90f88e2d4b79c3e1d79bc11a0709b3a61
Max Trash <max@deadend.xyz>
2022-09-06 21:30:44 +0300

    Add ABOUT section

---
+++ MC_about_game.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+
+
+func _on_link_godot_pressed():
+	OS.shell_open("https://godotengine.org")
+
+
+func _on_link_git_hub_pressed():
+	OS.shell_open("https://github.com/trash-max/classic_snake_4")
+
+
+func _on_link_music_pressed():
+	OS.shell_open("https://opengameart.org/content/we-are-prophet-happy-energetic-tune")
+
+
+func _on_b_close_pressed():
+	hide()
